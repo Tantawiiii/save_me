943661e594c55f9e05ec05262f0b3d49af80af4b
Ahmed Ramadan <ahmedtramadan4@gmail.com>
2023-11-16 16:04:36 +0200

    create design for profile and Setting Screens

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -9,12 +9,7 @@ class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
-
 class _HomeScreenState extends State<HomeScreen> {
-
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
